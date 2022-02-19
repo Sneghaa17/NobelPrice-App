b4cefc067ed511ec3035f3adc87f593c02d92ec5
Sneghaa17 <90645961+Sneghaa17@users.noreply.github.com>
2022-02-19 11:26:05 +0530

    Create Scratch.java

---
+++ Scratch.java
@@ -0,0 +1,17 @@
+package net.androidsquad.prizejson;
+import...
+public class MainActivity extends AppCompactActivity {
+
+    private static String JSON_URL =  "https://run.mocky.io/v3/7c467721-5754-48ff-9a30-5ca3444661ab";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    }
+
+}
+
